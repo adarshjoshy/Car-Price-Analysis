792a4ffa1a2eef1e32dfedf9017a660375247f8e
Adarsh Joshy <adarshjoshy@hotmail.com>
2020-06-05 02:47:15 +0530

    Added hyperparameter tuning to RF

--- Random_Forests_Model.py
+++ Random_Forests_Model.py
@@ -10,9 +10,25 @@ class CustomRandomForest:
         from sklearn.ensemble import RandomForestRegressor
         
         self.regressor = RandomForestRegressor()
+
+    def tune_parameters(self, X_train, y_train):
+        from sklearn.model_selection import GridSearchCV
+        
+        parameters = {'n_estimators': [75, 100, 200, 250], 'max_depth': [5, 10, 15, 20]}
+        regressor_ = GridSearchCV(self.regressor, parameters, cv=5, n_jobs=-1)
+        regressor_.fit(X_train, y_train)
+        
+        self.best_parameters = regressor_.best_params_
+        self.best_score = regressor_.best_score_
+        
+        print('Best parameters : {}'.format(self.best_parameters))
+        print('Best score : {}'.format(self.best_score))
     
     def fit(self, X_train, y_train):
+        from sklearn.ensemble import RandomForestRegressor
         
+        self.regressor = RandomForestRegressor(n_estimators = self.best_parameters['n_estimators'], 
+                                               max_depth = self.best_parameters['max_depth'])
         self.model = self.regressor.fit(X_train, y_train)
         
     def predict(self, X_test):
@@ -31,5 +47,4 @@ class CustomRandomForest:
                   ['Mean Squared Error', mse_rf]]
         df = pd.DataFrame(scores, columns=['Metrics', 'Score'])
         return df
-
 
